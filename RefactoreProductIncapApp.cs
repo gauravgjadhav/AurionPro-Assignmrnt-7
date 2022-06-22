c5c9026369ffb68baab7b606b51d4e2c1740a3be
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-22 15:56:33 +0530

    Add files via upload

---
+++ RefactoreProductIncapApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RefactoreProductIncapApp.Model;
+class program
+{
+    public static void Main()
+    {
+        Product rice;
+        rice = new Product();
+        rice.SetId(1001);
+        rice.SetName("Rice");
+        rice.SetPrice(85);
+        rice.SetDiscountPercentage(20);
+        Console.WriteLine("Id is {0}\nProduct name is {1}\nThe price is: {2}.Kg\n Discount Percentage is: {3}%\nAfter Discount Price is: {4}", rice.GetId(),rice.GetName(),rice.GetPrice(),rice.GetDiscountPercentage(),rice.DiscountCalculate());
+        Console.WriteLine();
+        Product sugar;
+        sugar = new Product();
+        sugar.SetId(1002);
+        sugar.SetName("Sugar");
+        sugar.SetPrice(40);
+        sugar.SetDiscountPercentage(101);
+        Console.WriteLine("Id is {0}\nProduct name is {1}\nThe price is: {2}\n Discount Percentage is: {3}%\nAfter Discount Price is: {4}", sugar.GetId(), sugar.GetName(), sugar.GetPrice(), sugar.GetDiscountPercentage(), sugar.DiscountCalculate());
+
+    }
+
+
+}
---
+++ StudentEncapApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StudentEncapApp.Model;
+class program
+{
+    public static void Main(string[] args)
+    {
+        Student stud;
+        stud = new Student();
+        stud.SetName("Ram");
+        stud.SetCgpa(5.6);
+        stud.SetRollno(1001);
+        Console.WriteLine("The name is {0}\nThe CGPA is {1}\nThe roll no is {2}\nThe CGPA percentage is {3}%",stud.GetName(),stud.GetCgpa(),stud.GetRillno(),stud.CgpaPecentage());
+
+    }
+}
