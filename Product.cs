737a23a6392ea644d4a94af84b482a413556dac0
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-07-02 19:56:52 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoreProductIncapApp.Model
+{
+    internal class Product
+    {
+        private int _id = 0;
+        private string _name = null;
+        private int _price = 0;
+        private double _discountPercentage = 0;
+        public double discontPrice = 0;
+        public double calculatePrice = 0;
+
+        public void SetId(int id)
+        {
+            if(id<0)
+            {
+                throw new ArgumentException("not valid");
+                
+            }
+            else
+            {
+                this._id = id;
+            }
+        }
+        public void SetName(string name)
+        {
+            name.ToUpper();
+            if (name.Length <1)
+            {
+                Exception exception = new ArgumentException("Name is Empty");
+                
+            }
+            else
+            {
+                this._name = name;
+            }
+        }
+        public void SetPrice(int price)
+        {
+            this._price = price;
+
+        }
+        public void SetDiscountPercentage(int percentage)
+        {
+            if(percentage<=0)
+            {
+
+                Exception exception = new ArgumentException("not a valid value");
+            }
+            else if (percentage >= 100) 
+            {
+
+                Exception exception = new ArgumentException("not a valid value");
+
+            }
+            else
+            {
+                this._discountPercentage = percentage;
+            }
+        }
+        public int GetId()
+        {
+            return this._id;
+        }
+        public string GetName()
+        {
+            return this._name;
+        }
+        public int GetPrice()
+        {
+            return this._price;
+        }
+        public double GetDiscountPercentage()
+        {
+            return this._discountPercentage;
+
+        }
+        public double DiscountCalculate()
+        {
+            _discountPercentage = _discountPercentage / 100;
+            discontPrice = _price * _discountPercentage;
+            calculatePrice = _price - discontPrice;
+            return calculatePrice;
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapApp.Model
+{
+    internal class Student
+    {
+        private int _rollno = 0;
+        private string _name = null;
+        private double _cgpa = 0;
+
+        public void SetName(string name)
+        {
+            if (name.Length <= 5)
+            {
+                this._name = name;
+            }
+            else
+            {
+                this._name = "Empty ";
+            }
+        }
+        public void SetCgpa(double cgpa)
+        {
+            if (cgpa < 1)
+            {
+                _cgpa = 1;
+            }
+            else if (cgpa > 10)
+            {
+                _cgpa = 10;
+            }
+            else
+            {
+                this._cgpa = cgpa;
+            }
+        }
+        public void SetRollno(int rollno)
+        {
+            if (rollno < 1)
+            {
+                _rollno = 000;
+            }
+            else
+            {
+                this._rollno = rollno;
+            }
+        }
+        public double CgpaPecentage()
+        {
+            return _cgpa = _cgpa * _cgpa;
+        }
+        public string GetName()
+        {
+            return this._name;
+        }
+        public double GetCgpa()
+        {
+            return this._cgpa;
+        }
+        public int GetRillno()
+        {
+            return this._rollno;
+        }
+        
+    }
+}
